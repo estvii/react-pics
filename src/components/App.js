43ee32cd98e4604a8ba77ebbde86d6aa532dd78c
Steven Vi <visteven00@gmail.com>
2019-05-01 22:28:20 +1000

    added docker files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import unsplash from './../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends Component {
-    state = {images: []};
+    state = { images: [] };
 
     onSearchSubmit = async (term) => {
         const response = await unsplash.get('/search/photos', {
@@ -12,14 +12,14 @@ class App extends Component {
                 query: term
             }
         });
-        this.setState({images: response.data.results})
+        this.setState({ images: response.data.results })
     }
 
     render() {
-        return(
-            <div className="ui container" style={{marginTop: '10px'}}> 
-                <SearchBar onSubmit={this.onSearchSubmit}/>
-                <ImageList images={this.state.images}/> 
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
                 Found: {this.state.images.length} images
             </div>
         );
